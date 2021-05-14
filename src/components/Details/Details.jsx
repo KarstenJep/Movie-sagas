e61b24158cd409c05153a9de8a4cab8df5f28ef2
Karsten Jepsen <kjepsen86@gmail.com>
2021-05-14 17:33:50 -0500

    Set up 'Back to List' button and routing

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,9 +1,21 @@
+import { useHistory } from 'react-router-dom';
 
 
 function Details() {
+
+    const history = useHistory();
+
     
+    const handleButton = () => {
+        console.log('Clicked Back to List button');
+        history.push('/');
+    }
+
     return (
-<h1>oh hay</h1>
+        <>
+        <h1>oh hay</h1>
+        <button className="button" onClick={handleButton}>Back to List</button>
+        </>
     )
 }
 
