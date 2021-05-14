d69589d88ed6ea2784c04343343ac9411bbf3307
Karsten Jepsen <kjepsen86@gmail.com>
2021-05-14 17:33:25 -0500

    Set up Cancel button and routing

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,9 +1,21 @@
+import { useHistory } from 'react-router-dom';
 
 
 function AddMovie() {
+
+    const history = useHistory();
+
     
+    const handleButton = () => {
+        console.log('Clicked Add Movie button');
+        history.push('/');
+    }
+
     return (
-<h1>Ope, wrong page</h1>
+        <>
+        <h1>Ope, wrong page</h1>
+        <button className="button" onClick={handleButton}>Cancel</button>
+        </>
     )
 }
 
